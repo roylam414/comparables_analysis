be97c0bf1e1b8bf5ce5f4207a9d985267ba587d6
Roy Lam <roylam414@gmail.com>
2020-07-30 15:37:35 +0800

    Put try/except to the loop to prevent sudden stop when error occurs

--- comparables_sorting.py
+++ comparables_sorting.py
@@ -20,41 +20,55 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 
 for i in stock_lists:
-
-    driver = webdriver.Chrome(options=chrome_options)
-    r = driver.get("https://www.hkex.com.hk/Market-Data/Securities-Prices/Equities/Equities-Quote?sym={}&sc_lang=en".format(i))
-    content = driver.page_source
-    soup = BeautifulSoup(content,'html.parser')
-
-    col_name = soup.find('p', class_='col_name').get_text()
-
-    col_last = soup.find('span', class_='col_last').get_text()
-    col_last = float(col_last)
+    try:
+        driver = webdriver.Chrome(options=chrome_options)
+        r = driver.get("https://www.hkex.com.hk/Market-Data/Securities-Prices/Equities/Equities-Quote?sym={}&sc_lang=en".format(i))
+        content = driver.page_source
+        soup = BeautifulSoup(content,'html.parser')
     
-    col_issued_shares = soup.find('span', class_='col_issued_shares').get_text()
-    col_issued_shares = col_issued_shares.split()[0]
-    col_issued_shares = float(re.sub(',','',col_issued_shares))
+        col_name = soup.find('p', class_='col_name').get_text()
     
-    market_cap = col_last * col_issued_shares
-    
-    PEratio = soup.find('dt', class_='ico_data col_pe').get_text()
-    if PEratio == '-':
-        PEratio = PEratio.replace('-','N/A')
-        earnings = 'N/A'
+        col_last = soup.find('span', class_='col_last').get_text()
+        col_last = float(col_last)
         
-       
-    else:
-        PEratio = float(PEratio.replace('x',''))
-        earnings = (col_last/PEratio)*col_issued_shares
-    
+        col_issued_shares = soup.find('span', class_='col_issued_shares').get_text()
+        col_issued_shares = col_issued_shares.split()[0]
+        col_issued_shares = float(re.sub(',','',col_issued_shares))
+        
+        market_cap = col_last * col_issued_shares
+        
+        PEratio = soup.find('dt', class_='ico_data col_pe').get_text()
+        if PEratio == '-':
+            PEratio = PEratio.replace('-','N/A')
+            earnings = 'N/A'
+            
+           
+        else:
+            PEratio = float(PEratio.replace('x',''))
+            earnings = (col_last/PEratio)*col_issued_shares
+          
     
 
-    col_summary = soup.find('div', class_='company_txt col_summary').get_text()
-   
-    lists = [[i,col_name, col_last, col_issued_shares,market_cap,PEratio, earnings, col_summary]]
-    df_temp = pd.DataFrame(lists, index=None, columns = columns)
-    df = df.append(df_temp, ignore_index=True)
-    driver.close()
+        col_summary = soup.find('div', class_='company_txt col_summary').get_text()
+       
+        lists = [[i,col_name, col_last, col_issued_shares,market_cap,PEratio, earnings, col_summary]]
+        df_temp = pd.DataFrame(lists, index=None, columns = columns)
+        df = df.append(df_temp, ignore_index=True)
+        driver.close()
+
+    except:
+        col_name = i
+        col_last = ''
+        col_issued_shares =''
+        market_cap = 0.00          ##Must be zero, if not below sorting won't work
+        earnings = ''
+        PEratio = ''
+        col_summary = ''
+        lists = [[i,col_name, col_last, col_issued_shares,market_cap,PEratio, earnings, col_summary]]
+        df_temp = pd.DataFrame(lists, index=None, columns = columns)
+        df = df.append(df_temp, ignore_index=True)
+        driver.close()
+        pass
 
 df = df.sort_values(by='market_cap', ascending=False)
 
